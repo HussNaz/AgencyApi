468b7bc56e71b3ed852b23d8465fce782184a7e8
HussNaz <nazmulhassanc09@gmail.com>
2024-11-12 13:31:30 +0600

    Commit for task-13

--- src/main/java/com/example/AgencyApi/model/Agency.java
+++ src/main/java/com/example/AgencyApi/model/Agency.java
@@ -1,10 +1,30 @@
 package com.example.AgencyApi.model;
 
+
+import jakarta.validation.constraints.NotBlank;
+
 public class Agency {
+
+    @NotBlank(message = "ID can not be blank")
     private String id;
+
+    @NotBlank(message = "Name can not be blank")
     private String name;
+
+    @NotBlank(message = "Code can not be blank")
     private String code;
+
     private String description;
+
+    public String getDescriptionBdd() {
+        return descriptionBdd;
+    }
+
+    public void setDescriptionBdd(String descriptionBdd) {
+        this.descriptionBdd = descriptionBdd;
+    }
+
+    private String descriptionBdd;
 
 
     public String getId() {
